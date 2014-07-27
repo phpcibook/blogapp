280450ed993d8e0c713ba3047595f1a5b93a4d4a
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-07-27 11:54:35 +0000

    CakePHPの土台を追加

---
+++ app/Config/bootstrap/environments/development.php
@@ -0,0 +1,11 @@
+<?php
+
+    Environment::configure('development', true, [  // (1) 
+        'MYSQL_DB_HOST'  => 'localhost',           // (2)
+        'MYSQL_USERNAME' => 'webapp',              // (3)
+        'MYSQL_PASSWORD' => 'passw0rd',            // (4)
+        'MYSQL_DB_NAME'  => 'blog',                // (5)
+        'MYSQL_TEST_DB_NAME' => 'test_blog',       // (6)
+        'MYSQL_PREFIX'   => '',                    // (7)
+    ]);
+
---
+++ app/Config/bootstrap/environments/test.php
@@ -0,0 +1,11 @@
+<?php
+
+    Environment::configure('test', false, [
+        'MYSQL_DB_HOST'  => 'localhost',
+        'MYSQL_USERNAME' => 'webapp',
+        'MYSQL_PASSWORD' => 'passw0rd',
+        'MYSQL_DB_NAME'  => 'test_blog',      // (1)
+        'MYSQL_TEST_DB_NAME' => 'test_blog',
+        'MYSQL_PREFIX'   => '',
+    ]);
+
