a2db64c35eb4d27522525543419ee6ec668493e3
Kaz Watanabe <kaz@decr.jp>
2014-09-14 07:51:30 +0000

    新規投稿、記事一覧のルーティングを追加

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -11,16 +11,8 @@
  * @since         CakePHP(tm) v 0.2.9
  */
 
-/**
- * Here, we are connecting '/' (base path) to controller called 'Pages',
- * its action called 'display', and we pass a param to select the view file
- * to use (in this case, /app/View/Pages/home.ctp)...
- */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
-/**
- * ...and connect the rest of 'Pages' controller's URLs.
- */
-	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+	Router::connect('/blogs/new', ['controller' => 'posts', 'action' => 'add']);
+	Router::connect('/:user_account/blog', ['controller' => 'posts', 'action' => 'index'], ['user_account']);
 
 /**
  * Load all plugin routes. See the CakePlugin documentation on
