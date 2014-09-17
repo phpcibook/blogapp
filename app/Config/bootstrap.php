ef0eff30016ae68e1f26c3f5bb03dcf5562b68e5
Kaz Watanabe <kaz@decr.jp>
2014-09-17 13:25:06 +0000

    Usersプラグインを追加

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -65,6 +65,9 @@ include dirname(__FILE__) . DS . 'bootstrap' . DS . 'environments.php';
  */
 CakePlugin::load('Migrations');
 CakePlugin::load('BoostCake');
+CakePlugin::load('Users');
+CakePlugin::load('Utils');
+CakePlugin::load('Search');
 
 /**
  * You can attach event listeners to the request lifecycle as Dispatcher Filter . By default CakePHP bundles two filters:
